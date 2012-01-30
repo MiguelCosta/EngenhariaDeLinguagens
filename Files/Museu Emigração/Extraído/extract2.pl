7be41466b5e09f3f530849b755e7975e3d93bc51
Bruno Azevedo <azevedo.252@gmail.com>
2012-01-30 18:43:51 +0000

    gravuras segundo cdwa lite feito

--- Files/Museu Emigração/Extraído/extract2.pl
+++ Files/Museu Emigração/Extraído/extract2.pl
@@ -10,7 +10,7 @@ use Switch;
 
 #my $origin = "word/fundo_museografico_MI_gravuras.doc" ;
 my $origin = shift(@ARGV);
-my $destiny =  "gravuras4.xml";
+my $destiny =  "gravurasLite.xml";
 
 # Perl trim function to remove whitespace from the start and end of the string
 sub trim($){
@@ -20,57 +20,75 @@ sub trim($){
 	return $string;
 }
 
+# Actualiza a hash que contem os diferentes campos de um objecto
 sub getValue {
-	my %cdwa = shift;
+	my $cdwa = shift;
 	my $tag = shift;
 	my $val = shift;
 
 	switch($tag) {
-		case "dt" { $cdwa{"dt"}=$val }
-		case "ap" { $cdwa{"ap"}=$val }
-		case "dat" { $cdwa{"dat"}=$val }
-		case "dim" { $cdwa{"dim"}=$val }
-		case "ni" { $cdwa{"ni"}=$val }
-		case "desc" { $cdwa{"desc"}=$val }
-		case "imp" { $cdwa{"imp"}=$val }
-		case "pi" { $cdwa{"pi"}=$val }
+		case "dt" { $cdwa->{"dt"}=$val }
+		case "ap" { $cdwa->{"ap"}=$val }
+		case "dat" { $cdwa->{"dat"}=$val }
+		case "dim" { $cdwa->{"dim"}=$val }
+		case "ni" { $cdwa->{"ni"}=$val }
+		case "desc" { $cdwa->{"desc"}=$val }
+		case "imp" { $cdwa->{"imp"}=$val }
+		case "pi" { $cdwa->{"pi"}=$val }
 		else {}
 	}
 
-	return %cdwa;
+	return %$cdwa;
 }
 
+# Devolve o esqueleto e respectivo conteudo para um objecto
 sub	 printBody {
-	my %cdwa = shift;
+	my $cdwa = shift;
+
 	my $body = "<descriptiveMetadata>";
-	$body .= "<objectWorkTypeWrap><objectWorkType>".$cdwa{"dt"} unless undef $cdwa{"dt"}."</objectWorkType></objectWorkTypeWrap>";
-	$body .= "<titleWrap><titleSet><title>".$cdwa{"dt"} unless undef $cdwa{"dt"}."</title></titleSet></titleWrap>";
-	$body .= "<displayCreator>".$cdwa{"ap"} unless undef $cdwa{"ap"}."</displayCreator>";
-	$body .= "<indexingCreatorWrap><indexingCreatorSet><nameCreatorSet><nameCreator>".$cdwa{"ap"} unless undef $cdwa{"ap"}."</nameCreator></nameCreatorSet><roleCreator/></indexingCreatorSet></indexingCreatorWrap>";
-	$body .= "<displayMeasurements>".$cdwa{"dim"}."</displayMeasurements>" unless undef $cdwa{"dim"};
-	if (not undef $cdwa{"dim"} and $cdwa{"dim"} =~ /([0-9][.,]?[0-9]*)\s*[xX]\s*([0-9][.,]?[0-9]*)\s*(\w+)/){
+	$body .= "<objectWorkTypeWrap><objectWorkType>".$cdwa->{"dt"}."</objectWorkType></objectWorkTypeWrap>";
+	$body .= "<titleWrap><titleSet><title>".$cdwa->{"dt"}."</title></titleSet></titleWrap>";
+	$body .= "<displayCreator>".$cdwa->{"ap"}."</displayCreator>";
+	$body .= "<indexingCreatorWrap><indexingCreatorSet><nameCreatorSet><nameCreator>".$cdwa->{"ap"}."</nameCreator></nameCreatorSet><roleCreator/></indexingCreatorSet></indexingCreatorWrap>";
+	$body .= "<displayMeasurements>".$cdwa->{"dim"}."</displayMeasurements>" if ($cdwa->{"dim"} ne "");
+	if ($cdwa->{"dim"} =~ /([0-9][.,]?[0-9]*)\s*[xX]\s*([0-9][.,]?[0-9]*)\s*(\w+)/){
 		$body .= "<indexingMeasurementsWrap><indexingMeasurementsSet>";
-		$body .= "<measurementsSet xmlns:ns3=\"http://www.getty.edu/CDWA/CDWALite\" ns3:value=\"".$1."\"ns3:unit=\"".$3."\" ns3:type=\"largura\"/>";
-		$body .= "<measurementsSet xmlns:ns3=\"http://www.getty.edu/CDWA/CDWALite\" ns3:value=\"".$2."\"ns3:unit=\"".$3."\" ns3:type=\"altura\"/>";
+		$body .= "<measurementsSet xmlns:ns3=\"http://www.getty.edu/CDWA/CDWALite\" ns3:value=\"".$1."\" ns3:unit=\"".$3."\" ns3:type=\"largura\"/>";
+		$body .= "<measurementsSet xmlns:ns3=\"http://www.getty.edu/CDWA/CDWALite\" ns3:value=\"".$2."\" ns3:unit=\"".$3."\" ns3:type=\"altura\"/>";
 		$body .= "</indexingMeasurementsSet></indexingMeasurementsWrap>";
 	}
 	$body .= "<displayMaterialsTech/>";
-	$body .= "<displayCreationDate>".$cdwa{"dat"} unless undef $cdwa{"dat"}."</displayCreationDate>";
+	$body .= "<displayCreationDate>".$cdwa->{"dat"}."</displayCreationDate>";
 	my $ano = "";
-	if (not undef $cdwa{"dat"} and $cdwa{"dat"} =~ /([0-9]{4})/) {$ano=$1}
+	if ($cdwa->{"dat"} =~ /([0-9]{4})/) {$ano=$1}
 	$body .= "<indexingDatesWrap><indexingDatesSet><earliestDate>$ano</earliestDate><latestDate>$ano</latestDate></indexingDatesSet></indexingDatesWrap>";
 	$body .= "<locationWrap><locationSet><locationName/></locationSet></locationWrap>";
-	$body .= "<descriptiveNoteWrap><descriptiveNoteSet><descriptiveNote>".$cdwa{"desc"} unless undef $cdwa{"desc"}."</descriptiveNote></descriptiveNoteSet></descriptiveNoteWrap>";
-	$body .= "<provenanceWrap><provenanceSet><provenanceDescription>".$cdwa{"pi"}."</provenanceDescription></provenanceSet></provenanceWrap>" unless undef $cdwa{"pi"};
+	$body .= "<descriptiveNoteWrap><descriptiveNoteSet><descriptiveNote>".$cdwa->{"desc"}."</descriptiveNote></descriptiveNoteSet></descriptiveNoteWrap>" if ($cdwa->{"desc"} ne "");
+	if ($cdwa->{"imp"} ne "") {
+		$cdwa->{"imp"} =~ /([^0-9]*)(.*)/; # encontra objecto onde foi publicado e as respectivas datas/nr
+		my @pub_anos = split /;|\s\/\s/, $2; # separa as datas das diversas publicacoes
+		my $pub = trim($1);
+		$body .= "<relatedWorksWrap>";
+		if (scalar @pub_anos == 0) { # casa nao possua informacao de quando foi impressa
+			$body .= "<relatedWorkSet><relatedWorkRelType>impresso em</relatedWorkRelType>";
+			$body .= "<labelRelatedWork>$pub</labelRelatedWork></relatedWorkSet>";
+		}
+		foreach (@pub_anos){ # para cada ano que foi impresso
+			$body .= "<relatedWorkSet><relatedWorkRelType>impresso em</relatedWorkRelType>";
+			my $ano_edi = trim($_);
+			$body .= "<labelRelatedWork>$pub; publicado $ano_edi</labelRelatedWork></relatedWorkSet>";
+		}
+		$body .= "</relatedWorksWrap>";
+	}
+	$body .= "<provenanceWrap><provenanceSet><provenanceDescription>".$cdwa->{"pi"}."</provenanceDescription></provenanceSet></provenanceWrap>" if ($cdwa->{"pi"} ne "");
 	$body .= "</descriptiveMetadata>";
 	$body .= "<administrativeMetadata>";
-	$body .= "<recordWrad><recordID>".$cdwa{"ni"}."</recordID><recordType/></recordWrap>";
+	$body .= "<recordWrap><recordID>".$cdwa->{"ni"}."</recordID><recordType/></recordWrap>";
 	$body .= "</administrativeMetadata>";
-
 
 	return $body;
 }
-
+# Escreve para um ficheiro xml ($destiny) o documento Word estruturado segundo o cdwa-lite. Invoca funcoes auxiliares.
 sub writeGravs {
 	my $gravura_flat = shift;
 	my %cdwa = ();
@@ -78,21 +96,21 @@ sub writeGravs {
 	open (F, ">:utf8",$destiny);
 	print F "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 	print F "<cdwaliteWrap xmlns=\"http://www.getty.edu/CDWA/CDWALite\"
-	    xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
-		    xsi:schemaLocation=\"http://www.getty.edu/CDWA/CDWALite file:/home/bruno/Documents/MEI/EL/PI/Engenharia-de-Linguagens---Projeto-Integrado/Files/Museu%20Emigra%C3%A7%C3%A3o/CDWALite-xsd-public-v1-1.xsd\">\n";
+	xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"
+	xsi:schemaLocation=\"http://www.getty.edu/CDWA/CDWALite file:/home/bruno/Documents/MEI/EL/PI/Engenharia-de-Linguagens---Projeto-Integrado/Files/Museu%20Emigra%C3%A7%C3%A3o/CDWALite-xsd-public-v1-1.xsd\">\n";
 
 	my @campos_flat = split "\n", $gravura_flat;
 	if (scalar @campos_flat != 0) {
 		foreach(@campos_flat) {
 			if (/^\s*$/){
 				print F "<cdwalite>\n";
-				print F printBody(%cdwa);
+				print F printBody(\%cdwa);
 				print F "</cdwalite>\n";
 				%cdwa = ();
 			}
 			elsif($_ =~ /([^:]+):(.*)/) {
 				my $value = trim($2);
-				%cdwa = getValue(%cdwa, $1, $value);
+				%cdwa = getValue(\%cdwa, $1, $value);
 			}
 		}
 	}
@@ -100,7 +118,7 @@ sub writeGravs {
 	close F;
 }
 
-
+# Funcao que extrai o texto do documento Word, trata um pouco o texto e invoca outras funcoes que tratarao da escrita para XML
 sub wordToXML {
 	my $file = Text::Extract::Word->new($origin); # Passed either a file name or an open file handle, this constructor returns an instance that can be used to query the file contents.
 	my $body = $file->get_body(); # Returns the text for the main body of the Word document. This excludes headers, footers, and annotations.
