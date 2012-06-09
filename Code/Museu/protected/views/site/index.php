b3cc812da6798915f353742f3ff1d914246f2512
Miguel Costa <miguelpintodacosta@gmail.com>
2012-06-10 00:27:10 +0100

    mudança na página inicial

--- Code/Museu/protected/views/site/index.php
+++ Code/Museu/protected/views/site/index.php
@@ -1,14 +1,69 @@
 <?php $this->pageTitle=Yii::app()->name; ?>
 
-<h1>Bem vindo ao <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
-<p>You may change the content of this page by modifying the following two files:</p>
+<h1 align="center">
+	Bem vindo ao <i><?php echo CHtml::encode(Yii::app()->name); ?> </i>
+</h1>
+<blockquote>" (...) Le Museu da Emigração e das Comunidades, Fafe,
+	Portugal, joue un rôle de leader dans ce réseau international des
+	musées des migrations, en raison non seulement de l’importance
+	historique des migrations pour le Portugal et d’échanges concrets avec
+	nos partenaires brésiliens et français notamment, mais aussi du
+	dynamisme propre à ce musée. (...)." (Paul de Guchteneire, Carine
+	Rouah: UNESCO-IOM; Migration Museums Network) "varios de nuestros
+	contactos nos han indicado que el Museu da Emigração de Fafe es uno de
+	los mejores en este campo. (...) y fue precisamente Brian Lambkin,
+	presidente de AEMI, una de las personas que más nos recomendó que
+	estudiáramos el caso de Fafe" [...] "La contribucisn de la experiencia
+	de Fafe ha sido decisiva a la hora de proponer nosotros un modelo de
+	Museo de Emigracisn Vasca." (Andoni Martín Arce, Presidencia, Gobierno
+	Vasco</blockquote>
+<br />
+<br />
 
+
+<h2>O que somos?</h2>
+
+<table>
+	<tr>
+		<td>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;Um Museu de âmbito nacional constituído
+				por Núcleos Museológicos e Sítios Históricos da Emigração e do
+				Retorno, instalados em espaços físicos temáticos.</p>
+
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;Estes organizam um museu polinucleado e um
+				itinerário desenhado para a valorização de espólios e das memórias
+				associadas às migrações.</p>
+
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;Nestes lugares e sítios encontram-se os
+				acervos documentais e museológicos de cada um dos pólos, dando aos
+				contextos de origem a compreensão dos factores de emigração.</p>
+			<p>&nbsp;&nbsp;&nbsp;&nbsp;Neles se evidenciam os quadros de risco
+				inscritos nos processo da viagem como sendo o momento da ruptura
+				cultural.</p>
+		</td>
+		<td><img src="../../myFiles/Imagens/static_pages/Migrantes.jpg" /></td>
+	</tr>
+</table>
+
+
+
+<p>&nbsp;&nbsp;&nbsp;&nbsp;Inscreve as suas principais finalidades na
+	perspectiva do conhecimento da emigração portuguesa, nela incluindo o
+	fenómeno que se verificou para a África, bem como aquele que se
+	verifica nos nossos dias, detendo-se, particularmente:</p>
 <ul>
-	<li>View file: <code><?php echo __FILE__; ?></code></li>
-	<li>Layout file: <code><?php echo $this->getLayoutFile('main'); ?></code></li>
+	<li>na emigração para o Brasil (séc. XIX e primeiras décadas do XX)</li>
+	<li>na emigração para os países Europeus (2.ª metade do séc.XX)</li>
 </ul>
 
-<p>For more details on how to further develop this application, please read
-the <a href="http://www.yiiframework.com/doc/">documentation</a>.
-Feel free to ask in the <a href="http://www.yiiframework.com/forum/">forum</a>,
-should you have any questions.</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;Funda a sua existência no facto da mobilidade
+	geográfica constituir um fenómeno estrutural da sociedade portuguesa,
+	deixando marcas em todos os continentes, não excluindo a emergência da
+	imigração como uma nova expressão de mobilidade em Portugal.</p>
+<p>&nbsp;&nbsp;&nbsp;&nbsp;Por outro lado, o fenómeno da mobilidade
+	humana constituiu-se como a expressão mais significativa da
+	transformação social e cultural da humanidade, fazendo dos actores
+	migrantes os principais motores da história da civilização, onde se
+	experimentam, ao mesmo tempo, a tragédia e a grandeza humana.</p>
+<p></p>
+<p></p>
